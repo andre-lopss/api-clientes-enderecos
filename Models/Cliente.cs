80720ba777cc229b1383d12fa659462b0d8332a2
André Lopes <75704128+andre-lopss@users.noreply.github.com>
2023-01-14 17:55:27 -0300

    Criação do serviço de coneção, Migrations e criação do Banco

--- Models/Cliente.cs
+++ Models/Cliente.cs
@@ -5,6 +5,7 @@ namespace API.Models
         public int Id { get; set; }
         public string Nome { get; set; }
         public string Email { get; set; }
+        public DateTime DataCadastro { get; set; }
         public bool Ativo { get; set; }
         public List<Endereco> Enderecos { get; set; }
     }
